0f8833728be5dab5c48b700656a99f6ff2326482
atduffaut <alex.duffaut@gmail.com>
2023-04-23 22:39:52 -0400

    Adding gain, quailty, freq, and slope parameters

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -55,6 +55,10 @@ public:
     //==============================================================================
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
+    
+    // New variables declared here
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+    juce::AudioProcessorValueTreeState apvts {*this, nullptr, "Parameters", createParameterLayout()};
 
 private:
     //==============================================================================
